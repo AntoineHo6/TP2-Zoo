18e574dcd69fbcb531cc03b61b6a3911c9506d9b
Antoine Ho <antoineho6@gmail.com>
2019-04-18 19:43:24 -0400

    Ajouter la barre d'info au jeu.

--- TP2 Zoo/ZooTycoon.Designer.cs
+++ TP2 Zoo/ZooTycoon.Designer.cs
@@ -23,12 +23,21 @@
         /// le contenu de cette méthode avec l'éditeur de code.
         /// </summary>
         private void InitializeComponent() {
+            this.panel1 = new System.Windows.Forms.Panel();
             this.etatJeu1 = new TP2_Zoo.Etat.EtatJeu();
             this.SuspendLayout();
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(219)))), ((int)(((byte)(154)))), ((int)(((byte)(66)))));
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1280, 68);
+            this.panel1.TabIndex = 1;
+            // 
             // etatJeu1
             // 
-            this.etatJeu1.Location = new System.Drawing.Point(0, 0);
+            this.etatJeu1.Location = new System.Drawing.Point(0, 68);
             this.etatJeu1.Name = "etatJeu1";
             this.etatJeu1.Size = new System.Drawing.Size(1280, 832);
             this.etatJeu1.TabIndex = 0;
@@ -37,7 +46,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1280, 832);
+            this.ClientSize = new System.Drawing.Size(1280, 900);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.etatJeu1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.KeyPreview = true;
@@ -51,6 +61,7 @@
         #endregion
 
         private Etat.EtatJeu etatJeu1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
